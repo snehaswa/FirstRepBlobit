e57d5954eb67b340f6d86297e7a29b0197e54ba1
sneharajkumarswami <sneharajkumarswami@192.168.203.152>
2022-05-16 16:27:03 +0530

    Commiting new extent Report package

---
+++ Blobit/src/main/java/ExtentReportListner/ExtentReportNew.java
@@ -0,0 +1,76 @@
+package ExtentReportListner;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.ObjectInputFilter.Status;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.imageio.ImageIO;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
+import com.relevantcodes.extentreports.model.Screencast;
+
+public class ExtentReportNew {
+	ExtentHtmlReporter htmlReporter;
+	ExtentReports extent;
+
+	@BeforeSuite
+	public void setup() {
+		htmlReporter = new ExtentHtmlReporter("extent.html");
+
+		// create ExtentReports and attach reporter(s)
+		extent = new ExtentReports();
+		extent.attachReporter(htmlReporter);
+
+	}
+
+	@Test
+	public void test1() throws Exception {
+		// creates a toggle for the given test, adds all log events under it
+		ExtentTest test = extent.createTest("MyFirstTest", "Sample description");
+
+		//test.log(Status.values(), "This step shows usage of log(status, details)");
+
+		test.info("This step shows usage of info(details)");
+
+			}
+
+	
+
+	
+
+
+	public static String getScreenshot(WebDriver driver, String screenshotName) throws IOException {
+		String dateName = new SimpleDateFormat("yyyyMMddhhmmss").format(new Date());
+		TakesScreenshot ts = (TakesScreenshot) driver;
+		File source = ts.getScreenshotAs(OutputType.FILE);
+
+		// after execution, you could see a folder "FailedTestsScreenshots" under src
+		// folder
+		String destination = System.getProperty("user.dir") + "/Screenshots/" + screenshotName + dateName + ".png";
+		File finalDestination = new File(destination);
+		FileUtils.copyFile(source, finalDestination);
+		return destination;
+	}
+
+
+
+	@AfterSuite()
+	public void tearDown() {
+		extent.flush();
+
+	}
+}
